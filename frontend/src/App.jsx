f051ce0224643b1678062dc784ae3e663c1501c2
codebydarshan <darshandesai842004@gmail.com>
2026-06-07 12:55:17 +0530

    feat: add OrdersPage, useOrdersPage hook, and OrderPreview component for order management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,11 @@ import Layout from './components/Layout';
 import { Routes, Route, Navigate } from "react-router";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
+import OrdersPage from "./pages/OrdersPage";
 
 
 function App() {
-  const { isLoaded } = useAuth();
+  const { isLoaded, isSignedIn } = useAuth();
 
   if(!isLoaded) return <PageLoader />;
 
@@ -16,6 +17,11 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route
+          path="/orders"
+          element={isSignedIn ? <OrdersPage /> : <Navigate to={"/"} replace />}
+        />
+
       </Routes>
     </Layout>
   )
